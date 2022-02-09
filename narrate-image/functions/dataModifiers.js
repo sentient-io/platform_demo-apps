829fcfd2505b0ef95aa8cd52448f0186c8dbe1b7
Priyadharshini <57977344+priyadharshini-sent@users.noreply.github.com>
2022-02-09 20:05:34 +0530

    narrate-image

---
+++ narrate-image/functions/dataModifiers.js
@@ -0,0 +1,239 @@
+/* +-----------------------+ */
+/* | Group Detected Object | */
+/* +-----------------------+ */
+/* Convert returned value to organised group */
+groupDetectedObjects = (sParam) => {
+	return new Promise((resolve, reject) => {
+		let param
+		if (sParam.constructor == String) {
+			param = JSON.parse(sParam)
+		} else {
+			param = sParam
+		}
+		let result = {}
+		for (key in param) {
+			let object = param[key]['class']
+			let confidence = param[key]['confidence']
+			let sBoundingBox = param[key]['bounding box']
+			let location = {
+				left: sBoundingBox.left,
+				top: sBoundingBox.top,
+				right: sBoundingBox.right,
+				bottom: sBoundingBox.bottom,
+			}
+			let size = {
+				width: sBoundingBox.right - sBoundingBox.left,
+				height: sBoundingBox.bottom - sBoundingBox.top,
+			}
+			let detail = {
+				object: object,
+				confidence: confidence,
+				location: location,
+				size: size,
+			}
+			if (!result[`${object}`]) {
+				detail.id = 1
+				result[`${object}`] = {}
+				result[`${object}`][`${object}-1`] = detail
+			} else {
+				let id = Object.keys(result[`${object}`]).length + 1
+				detail.id = id
+				result[`${object}`][`${object}-${id}`] = detail
+			}
+		}
+		data.detectedObjects = result
+		resolve(result)
+	})
+}
+
+/* +--------------------+ */
+/* | Filter By Category | */
+/* +--------------------+ */
+/* Filter all detectd object by category */
+filterByCategory = (category, source) => {
+	let result = {}
+
+	category.forEach((index) => {
+		if (source[index]) {
+			result[`${index}`] = source[index]
+		}
+	})
+
+	return result
+}
+
+/* +---------------------------------------+ */
+/* | Group Detected Object into a sentence | */
+/* +---------------------------------------+ */
+narrateDetectedObjects = (sParam) => {
+	return new Promise((resolve, reject) => {
+		let sentence = ''
+		let be = Object.keys(sParam).length == 1 ? 'is' : 'are'
+		let categoryArry = Object.keys(sParam).sort()
+
+		if (categoryArry[0] === undefined) {
+			// Case : no detected object
+			sentence = 'Sorry, no object detected, please try another picture.'
+			state.noDetection = true
+		} else if (categoryArry.length === 1) {
+			// Case : 1 type of object detected
+			let objectCount = Object.values(sParam[categoryArry[0]]).length
+			objectCount > 1 ? (be = 'are') : be
+			let categoryName =
+				objectCount > 1 ? singularToPlural(categoryArry[0]) : categoryArry[0]
+			sentence =
+				'There' + ' ' + be + ' ' + objectCount + ' ' + categoryName + '.'
+		} else {
+			// Case : multiple types of object detected
+			categoryArry.forEach((category) => {
+				let objectCount = Object.values(sParam[category]).length
+				objectCount > 1 ? (be = 'are') : be
+				let categoryName =
+					objectCount > 1 ? singularToPlural(category) : category
+				let comma = ', '
+				let and = ''
+				// For second last item, remove comma
+				category == categoryArry[categoryArry.length - 2]
+					? (comma = ' ')
+					: comma
+				// For last item, chagne comma to fullstop, add "and"
+				category == categoryArry[categoryArry.length - 1]
+					? ((comma = '.'), (and = 'and '))
+					: comma
+				sentence += and + objectCount + ' ' + categoryName + comma
+			})
+			sentence = 'There' + ' ' + be + ' ' + sentence
+		}
+		data.description = sentence
+		resolve(sentence)
+	})
+}
+
+/* +-------------------------------+ */
+/* | Convert object name to plural | */
+/* +-------------------------------+ */
+singularToPlural = (word) => {
+	let esPlural = ['bus', 'bench', 'wine glass', 'sandwich']
+	let noPlural = ['scissors', 'sheep']
+	let vesPlural = ['knife']
+
+	word = $.trim(word)
+
+	if (word === 'person') {
+		return 'people'
+	} else if (word === 'mouse') {
+		return 'mice'
+	} else if (esPlural.includes(word)) {
+		return word + 'es'
+	} else if (vesPlural.includes(word)) {
+		return word.replace('fe', 'ves')
+	} else if (noPlural.includes(word)) {
+		return word
+	} else {
+		return word + 's'
+	}
+}
+
+window.playAudio = function () {
+	audio.crossOrigin = 'anonymous'
+	audio.play()
+}
+
+/* +----------------------+ */
+/* | Draw image to canvas | */
+/* +----------------------+ */
+canvasDrawImage = (base64string, sWidth, sHeight, resize = 100) => {
+	// Takes base64string, source Width and Height
+	// Resize will be the size of rendered canvas
+	let dWidth
+	let dHeight
+	if (sWidth >= sHeight) {
+		// When image is landscape
+		dWidth = resize
+		dHeight = (sHeight * dWidth) / sWidth
+	} else {
+		// When image is portrait
+		dHeight = resize
+		dWidth = (sWidth * dHeight) / sHeight
+	}
+
+	let canvas = document.createElement('canvas')
+	canvas.setAttribute('width', dWidth)
+	canvas.setAttribute('height', dHeight)
+
+	let ctx = canvas.getContext('2d')
+
+	var image = new Image()
+	image.onload = () => {
+		ctx.drawImage(image, 0, 0, sWidth, sHeight, 0, 0, dWidth, dHeight)
+	}
+	image.src = base64string
+
+	return canvas
+}
+
+/* +------------------------------------------+ */
+/* | Draw box to canvas based on bounding box | */
+/* +------------------------------------------+ */
+canvasDrawBox = (e) => {
+	return new Promise((resolve, reject) => {
+		let canvasID, x, y, width, height, object, resizeRatio
+		e.canvasID ? (canvasID = e.canvasID) : reject()
+		// Resize ratio will not be 1 if the preview image been resized
+		state.file.resizeRatio
+			? (resizeRatio = state.file.resizeRatio)
+			: (resizeRatio = 1)
+
+		x = e.left * resizeRatio
+		y = e.top * resizeRatio
+
+		// Prevent API from return negative value
+		if (y < 0) {
+			y = 0
+		} else if (x < 0) {
+			x = 0
+		}
+
+		width = e.right * resizeRatio - x
+		height = e.bottom * resizeRatio - y
+
+		width > 100 ? width : (width = 100)
+
+		e.object ? (object = e.object) : (object = '??')
+
+		let canvas = document.getElementById(canvasID)
+		height > canvas.offsetHeight - 80
+			? (height = canvas.offsetHeight - 80)
+			: height
+
+		let ctx = canvas.getContext('2d')
+		// Draw boxes
+		ctx.beginPath()
+		ctx.lineWidth = '1'
+		ctx.strokeStyle = '#00deff'
+		ctx.rect(x, y, width, height)
+		ctx.stroke()
+		// Draw text background
+		ctx.fillStyle = 'rgba(0,222,255,0.7)'
+		// x -1 and width + 2 to align with the stroke of bounding box
+		ctx.fillRect(x - 1, y, width + 2, 24)
+		// Draw text to canvas
+		ctx.fillStyle = '#052a30'
+		ctx.font = '14px sans-serif'
+		ctx.fillText(`${object} `, x - 1 + 5, y + 15)
+	})
+}
+
+/* +-------------------------------------+ */
+/* | Render description sentence to HTML | */
+/* +-------------------------------------+ */
+displayDescription = (sParam) => {
+	let description = document.createElement('div')
+	$(description).html(`<p>${sParam}</p>`)
+	$(description).addClass('s-img-narration-description')
+	$(description).attr(
+		'style',
+		`max-width:${data.file.sWidth * data.file.resizeRatio + 20}px`
+	)
+	$('#s-img-preview').append(description)
+}
---
+++ narrate-image/functions/eventListeners.js
@@ -0,0 +1,335 @@
+/** State to hold and update user inputs or selections */
+let state = {
+	userApiKey: apikey,
+	hasImage: false,
+	audioPlaying: false,
+	detectionDescription: '',
+}
+let data = {}
+/* +----------------+ */
+/* |  Img Uploader  | */
+/* +----------------+ */
+// 1MB is 1048576
+let fileSizeLimit = 1048576 * 5
+// Limit upload image resolution
+let imgDimensionLimit = 416
+
+let dropArea = document.getElementById('s-img-uploader')
+// Prevent default behaviors
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
+	dropArea.addEventListener(eventName, preventDefaults, false)
+})
+function preventDefaults(e) {
+	e.preventDefault()
+	e.stopPropagation()
+}
+// Highlight effect when drag files over
+;['dragenter', 'dragover'].forEach((eventName) => {
+	dropArea.addEventListener(eventName, highlight, false)
+})
+;['dragleave', 'drop'].forEach((eventName) => {
+	dropArea.addEventListener(eventName, unhighlight, false)
+})
+function highlight(e) {
+	dropArea.classList.add('highlight')
+}
+function unhighlight(e) {
+	dropArea.classList.remove('highlight')
+}
+
+//Get the data for the files that were dropped
+dropArea.addEventListener('drop', handleDrop, false)
+function handleDrop(e) {
+	let dt = e.dataTransfer
+	let files = dt.files
+	uploadImg(files)
+}
+
+// Handle picture preview
+uploadImg = (files) => {
+	state.hasImage = false
+	checkImgFormat({ inputFormat: files[0].type.split('/')[1] })
+		.then(() => {
+			// Preview uploaded file
+			let reader = new FileReader()
+			reader.readAsDataURL(files[0])
+			// Push uploaded image details to state
+			state.file = { name: files[0].name, type: files[0].type }
+			reader.onloadend = () => {
+				// Draw and resize uploaded image to canvas
+				previewImg(reader.result, 600)
+				// Push original base64 string to state
+				state.file.base64 = reader.result
+			}
+		})
+		.catch((error) => {})
+}
+
+previewImg = (src, previewPicSize = 600, files) => {
+	console.log(src)
+	let image = new Image()
+	image.src = src
+	image.onload = () => {
+		state.file.sWidth = image.width
+		state.file.sHeight = image.height
+		// Update canvasResizeRatio for resizing returned boxes
+		if (image.size >= fileSizeLimit) {
+			let errTitle = 'File Size Too Big'
+			let errMsg =
+				'For demp purpose, we are limiting file size to 5MB. Please try another image.'
+			toggleAlert(errTitle, errMsg)
+		} else if (
+			image.width < imgDimensionLimit ||
+			image.height < imgDimensionLimit
+		) {
+			let errTitle = 'Low Image Resolution'
+			let errMsg = `Upload image dimension: ${image.width} x ${image.height}px. <br> Please use image with at least ${imgDimensionLimit} x ${imgDimensionLimit}px.`
+			toggleAlert(errTitle, errMsg)
+		} else if (image.width >= image.height) {
+			// Prevent upscaling small images
+			if (image.width < previewPicSize) {
+				previewPicSize = image.width
+			}
+			state.file.resizeRatio = previewPicSize / image.width
+			state.hasImage = true
+		} else {
+			if (image.height < previewPicSize) {
+				previewPicSize = image.height
+			}
+			state.file.resizeRatio = previewPicSize / image.height
+			state.hasImage = true
+		}
+		if (state.hasImage) {
+			$('#s-img-uploader').hide()
+			// Remove existing preview canvas
+			$('#uploadedImg, #uploadedImgBase').remove()
+			// Toggle cancel and function button
+			$('#btn-main-function, #btn-upload-another').show()
+			// Src is uploaded file src to create image element
+			// previewPicSize : Number, determine the size of displayed iamge
+			let canvas = canvasDrawImage(
+				src,
+				image.width,
+				image.height,
+				previewPicSize
+			)
+
+			let baseCanvas = canvasDrawImage(
+				src,
+				image.width,
+				image.height,
+				previewPicSize
+			)
+
+			canvas.setAttribute('id', 'uploadedImg')
+			baseCanvas.setAttribute('id', 'uploadedImgBase')
+			$('#s-img-preview').append(canvas)
+			$('#s-img-preview-base').append(baseCanvas)
+			// Scroll to button area after image uploaded
+			window.scroll({
+				top: $('#s-img-preview').offset().top - 100,
+				left: 0,
+				behavior: 'smooth',
+			})
+		}
+	}
+}
+
+checkImgFormat = (param) => {
+	let inputFormat = param.inputFormat
+	let acceptedFromat = [
+		'bmp',
+		'dib',
+		'exr',
+		'hdr',
+		'jpeg',
+		'jpg',
+		'jpe',
+		'jp2',
+		'png',
+		'pic',
+		'pbm',
+		'pgm',
+		'ppm',
+		'pxm',
+		'pnm',
+		'ras',
+		'sr',
+		'tiff',
+		'tif',
+		'webp',
+	]
+	return new Promise((resolve, reject) => {
+		if (acceptedFromat.includes(inputFormat)) {
+			resolve()
+		} else {
+			let errTitle = 'Unsupported File Format'
+			let errMsg = `Uploader file foramt: ${inputFormat}. <br><br>Supported formats: bmp, dib, exr, hdr, jpeg, jpg, jpe, jp2, png, pic, pbm, pgm, ppm, pxm, pnm, ras, sr, tiff, tif, webp.`
+			toggleAlert(errTitle, errMsg)
+			// Clear record of uploaded file
+			$('#s-img-input').val('')
+			reject('Error, unsupported file format.')
+		}
+	})
+}
+
+selectImage = (e) => {
+	let base64 = e.src
+	let id = e.id
+	state.file = { base64: base64, name: id, type: 'image/jpeg' }
+
+	previewImg(e.src)
+}
+
+/* +-----------------+ */
+/* |  Narrate Image  | */
+/* +-----------------+ */
+handleImageProcessing = () => {
+	// Handle if no API key
+	if (!hasApiKey()) {
+		state.userApiKey = prompt(
+			'Unauthorized user, to continue test out, please provide a valid API key and try again.',
+			state.user
+		)
+		window.sessionStorage.setItem('sentientApiKey', state.userApiKey)
+
+		if (!hasApiKey()) {
+			return
+		}
+	}
+	// Return base64 string as data
+	let data = state.file.base64.split('base64,')[1]
+	loadingStart()
+	objectDetectionAPICall(data)
+		.then((result) => {
+			// Display detect objets button and display restart button
+			$(
+				'#btn-main-function, #sample-images-container, #btn-upload-another'
+			).hide()
+			$('#btn-restart').show()
+
+			// Draw result to canvas
+			Object.values(result).forEach((e) => {
+				let bbox = e['bounding box']
+				let detail = {
+					canvasID: 'uploadedImg',
+					top: bbox.top,
+					bottom: bbox.bottom,
+					left: bbox.left,
+					right: bbox.right,
+					object: e['class'],
+				}
+				canvasDrawBox(detail)
+			})
+			return groupDetectedObjects(result)
+		})
+		//make into a sentence
+		.then((result) => {
+			return narrateDetectedObjects(result)
+		})
+		//show and narrate the sentence
+		.then((result) => {
+			$('#detectionDescription').html(result)
+			$('#detectionDescription').show()
+			$('#toggleBoxes').show()
+			$('#btn-generateAudio').show()
+			state.detectionDescription = result
+			loadingEnd()
+		})
+		.catch((err) => {
+			// Toggle popup window
+			toggleAlert('Error', err)
+			loadingEnd()
+		})
+}
+
+function hasApiKey() {
+	if (
+		(state.userApiKey === null || state.userApiKey == '') &&
+		apikey == undefined
+	) {
+		return false
+	} else {
+		return true
+	}
+}
+
+function fetchApiKeyFromSessionStorage() {
+	const k = window.sessionStorage.getItem('sentientApiKey')
+	state.userApiKey = k
+}
+fetchApiKeyFromSessionStorage()
+
+/* +-------------------------+ */
+/* | Narrate the sentence    | */
+/* +-------------------------+ */
+handleAudio = () => {
+	$('#btn-main-function, #generateAudio').hide()
+	$('#in-line-loader').show()
+	textToSpeechAPICall(state.detectionDescription)
+		.then((result) => {
+			var audioString = 'data:audio/wav;base64,' + result.results['wav_base64']
+			var audio = document.getElementById('audio')
+			audio.src = audioString
+			audio.crossOrigin = 'anonymous'
+			$('#audio').show()
+		})
+		.catch((err) => {
+			toggleAlert('Error', err)
+		})
+		.finally(() => {
+			$('#in-line-loader, #btn-generateAudio').hide()
+		})
+}
+
+handleRestart = () => {
+	$('#s-img-preview, #s-img-preview-base').empty()
+	$(
+		'#btn-restart, #toggleBoxes, #btn-upload-another, #btn-main-function, #btn-generateAudio, #audio, #detectionDescription'
+	).hide()
+	$('#s-img-input').val('')
+	$('#s-img-uploader, #sample-images-container').show()
+}
+
+let loadingMsg = [
+	'Just a moment more, processing your file...',
+	'You can buy and sell data securely on Sentient.io’s blockchain network.',
+	'Use utility microservices to save time during your app development.',
+	'Have a microservice you\'re looking for but can\'t find? Write in to us <a style="text-decoration:underline"  href = "mailto: enquiry@sentient.io">enquiry@sentient.io</a>',
+	"Need help with implementing the APIs? Click the 'Help' button at the bottom of the screen to reach out to our support team.",
+	'The APIs on our platform are curated carefully to ensure reliability for deployment',
+	'Usage discounts are automatically applied as the number of API calls made reaches the next tier',
+	'Just a moment more, processing your file...',
+]
+
+let loading
+
+loadingStart = () => {
+	$('#loader').toggle()
+
+	let msgIndex = 0
+	loading = window.setInterval(() => {
+		$('#loader-text').html(loadingMsg[msgIndex])
+		if (msgIndex < loadingMsg.length) {
+			msgIndex += 1
+		} else {
+			msgIndex = 1
+		}
+	}, 4000)
+}
+
+loadingEnd = () => {
+	$('#loader').toggle()
+	window.clearInterval(loading)
+}
+
+// Toggle popup alert window
+toggleAlert = (alertTitle, alertMsg) => {
+	$('#alertContent').html(alertMsg)
+	$('#alertTitle').html(alertTitle)
+	$('#alert').modal('toggle')
+}
+
+handleToggleCanvasBox = () => {
+	$('#s-img-preview-container, #s-img-preview-base').toggle()
+}
